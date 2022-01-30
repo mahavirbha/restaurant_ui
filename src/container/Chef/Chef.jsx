82cbc37111a5f998cbe9c284b3af34bb0a5e655a
Mahavirbha <robowithsomeaix@gmail.com>
2022-01-30 14:48:24 +0530

    Chef Completed

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
-
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-rev'>
+      <img src={images.chef} alt='chef' />
+    </div>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chef's Word" />
+      <h1 className='headtext__cormorant'>What We Believe In</h1>
+
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+          <img src={images.quote} alt='quote' />
+          <p className='p__opensans'>Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Quis Pharetra Adipiscing</p>
+        </div>
+        <p className='p__opensans'>Ultrices Vulputate Posuere Tristique. In Sed Odio Nec Aliquet Eu Proin Mauris Et. Know More </p>
+      </div>
+
+      <div className='app__chef-sign'>
+        <p>kevin Luo</p>
+        <p className='p__opensans'>Chef & Founder</p>
+        <img src={images.sign} alt='sign'/>
+      </div>
+
+    </div>
   </div>
 );
 
